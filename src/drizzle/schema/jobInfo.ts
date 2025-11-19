69d9d4ccd0974b5b4204c22ee4b6ee1b97b7e6f0
Hassan Bachir <125279202+hassan-bachir@users.noreply.github.com>
2025-11-19 15:22:03 +0200

    feat: enhance database schema with interview and question tables, and update jobInfo relations

--- src/drizzle/schema/jobInfo.ts
+++ src/drizzle/schema/jobInfo.ts
@@ -2,6 +2,8 @@ import { pgEnum, pgTable, varchar } from "drizzle-orm/pg-core";
 import { createdAt, id, updatedAt } from "../schemahelpers";
 import { UserTable } from "./user";
 import { relations } from "drizzle-orm";
+import { QuestionTable } from "./question";
+import { InterviewTable } from "./interview";
 
 export const experienceLevels = ["junior", "mid-level", "senior"] as const;
 export type ExperienceLevel = (typeof experienceLevels)[number];
@@ -23,9 +25,11 @@ export const JobInfoTable = pgTable("job_info", {
   updatedAt,
 });
 
-export const jobInfoRelations = relations(JobInfoTable, ({ one }) => ({
+export const jobInfoRelations = relations(JobInfoTable, ({ one, many }) => ({
   user: one(UserTable, {
     fields: [JobInfoTable.userId],
     references: [UserTable.id],
   }),
+  questions: many(QuestionTable),
+  interviews: many(InterviewTable),
 }));
